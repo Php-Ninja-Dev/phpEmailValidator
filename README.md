5a7803e2adbac427422f0da7a224a0eab0789e7b
Beto <betolopezayesa@gmail.com>
2023-11-30 09:06:21 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # phpEmailValidator
 Php Email Validator. Validate any email
+
+
+# Getting Started
+
+include "emailValidator.php"; file
+
+then
+
+$emailValidator = new emailValidator();
+
+
+// Check $email
+$emailValidator->isValid($email);
+return true or false
---
+++ invalidEmails.php
@@ -0,0 +1,53 @@
+<?
+/*
+ * Package Name: Email Validator
+ * File Description: Hardcoded typical invalid emails
+ * 
+ * @author Beto Ayesa <beto.phpninja@gmail.com>
+ * @version 1.0.0
+ * @package PhpEmailValidator
+ * @license GPL3
+ * @link https://github.com/natzar/stripe-pad
+ * 
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    This file is part of Stripe Pad.
+
+	Stripe Pad is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+	Stripe Pad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with  Stripe Pad. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+$invalidEmails = [
+	'your@email.com',
+	'john.doe@gmail.com',
+	'email@address.com',
+	'ajax-loader@2x.gif',
+	'popper.js@1.15',
+	'addons4prestashop@gmail.com',
+	'arrow-left-white@2x.png',
+	'arrow-right-white@2x.png',
+	'dsd_100x@2x.png',
+	'email@ejemplo.com',
+	'email@email.com',
+	'customer@example.com',
+	'email@example.com',
+	'example@domain.com',
+	'logo_header@2x.png',
+	'mail@mail.com',
+	'name@domain.com'
+
+
+
+];
